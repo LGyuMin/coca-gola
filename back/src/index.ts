ca6f6a7067c766c6b12f2e19f2cb96ce60344831
규.이규민 <gmlee@lunasoft.co.kr>
2022-01-18 12:03:53 +0900

    소켓 작업

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,11 +1,31 @@
-import Koa, { Context } from 'koa';
+import Koa from 'koa';
+import { createServer } from 'http';
+import cors from '@koa/cors';
+import { Server } from 'socket.io';
+import { ServerToClientEvents, ClientToServerEvents, InterServerEvents, SocketData } from './socket/server';
 
 const app = new Koa();
+const httpServer = createServer(app.callback());
 
-app.use((ctx: Context) => {
-    ctx.body = 'Hello Coca-Gola';
-})
+// 웹소켓 cors 설정
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
 
-app.listen(4000, () => {
+// api cors 설정
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+io.on("connection", (socket) => {
+    console.log('a user connected');
+    socket.on('hello', () => {
+        console.log('Hi');
+    })
+});
+
+httpServer.listen(4000, () => {
     console.log('Listening to port 4000');
-})
+});
