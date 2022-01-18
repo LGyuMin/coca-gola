67d441c651ca7e8e5a9574e96ed4f2015cc8fe76
규.이규민 <gmlee@lunasoft.co.kr>
2022-01-18 12:03:25 +0900

    coca-goal front

---
+++ front/src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { io, Socket } from "socket.io-client";
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+function App() {
+  // const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:4000');
+
+  // const handleClick = () => {
+  //   socket.emit('hello');
+  // }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
